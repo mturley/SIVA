c024456a7fce334b69c9ea4f53955898b8c9feda
DMP9 <dylanperks23@gmail.com>
2018-04-12 00:17:51 +0100

    Add FileHandler

---
+++ SIVA.WebPanel/Backend/Handlers/FileHandler.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System.Linq;
+using System.Reflection;
+using SimpleServer.Handlers;
+using SimpleServer.Internals;
+using SIVA.WebPanel;
+
+namespace SIVA.Backend.Handlers
+{
+    public class FileHandler : IHandler
+    {
+        public const string RootNamespace = "SIVA.WebPanel.Frontend";
+
+        public bool HasFile(string file)
+        {
+            return Assembly.GetAssembly(typeof(FileHandler)).GetManifestResourceNames()
+                .Contains(RootNamespace + file.Replace('/', '.').TrimEnd('.'));
+        }
+
+        public bool CanHandle(SimpleServerRequest request)
+        {
+            return HasFile(request.RawUrl);
+        }
+
+        public void Handle(SimpleServerContext context)
+        {
+            if (context.Request.Method == "GET")
+            {
+                context.Response.OutputStream.WriteFile(context.Request.RawUrl);
+            }
+            else
+            {
+                context.Response.StatusCode = 405;
+                context.Response.ReasonPhrase = "Method Not Allowed";
+                var sw = new StreamWriter(context.Response.OutputStream);
+                sw.Write("<h1>405 Method Not Allowed</h1>");
+                sw.Flush();
+                context.Response.Close();
+            }
+        }
+    }
+}
---
+++ SIVA.WebPanel/Helper.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using System.Reflection;
+using OAuth2.Client.Impl;
+using SIVA.Backend.Handlers;
+
+namespace SIVA.WebPanel
+{
+    public static class Helper
+    {
+        public static void WriteFile(this Stream s, string filename)
+        {
+            Assembly.GetAssembly(typeof(FileHandler)).GetManifestResourceStream(filename).CopyTo(s);
+        }
+    }
+}
