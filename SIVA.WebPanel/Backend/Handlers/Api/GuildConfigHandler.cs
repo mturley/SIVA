8e1381de594cc6c1e491d2d20b56c338521826e5
Greem Husted <gr33m11@gmail.com>
2018-04-14 11:36:32 -0400

    k

--- SIVA.WebPanel/Backend/Handlers/Api/GuildConfigHandler.cs
+++ SIVA.WebPanel/Backend/Handlers/Api/GuildConfigHandler.cs
@@ -33,6 +33,18 @@ namespace SIVA.WebPanel.Backend.Handlers.Api
             // - WelcomeColour: <colour as hex>
             // - Embed Colour: <input type="color" id="html5colorpicker" class="" style="background: rgba(0,0,0,0); border: none;" value="#<colour as hex>" style="width:85%;" onChange="updateAllColours();">
             // -               or <a href="">Unavailable</a>
+
+            /* JUST SO YOU KNOW, PERKS:
+             *      The namespace for managing JSON files is SIVA.Core.JsonFiles
+             *      The class is GuildConfig
+             *      The methods are GetOrCreateConfig, GetGuildConfig, SaveGuildConfig, and CreateGuildConfig
+             *      To get the config for a guild, simply do `var config = GuildConfig.GetGuildConfig(Server ID)`
+             *      That will return a list of all values you can modify. Use config.{ConfigOption} to access/set the values
+             *      Use GuildConfig.SaveGuildConfig(); to save the new config. The values you can use are stored in the SIVA.Core.JsonFiles.GuildConfig file, 
+             *      at the first class defined.
+             *      any questions just ask me
+             *                                          -Greem
+             */
         }
     }
 }
